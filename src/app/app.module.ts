7a9d527c2c360644ffcefd181b17188feb51e8fa
Taras <tarasparakhin@gmail.com>
2022-09-14 18:19:06 +0300

    3.4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserCardComponent } from './header/user-card/user-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserCardComponent
   ],
   imports: [
     BrowserModule
