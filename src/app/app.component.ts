ac153ba70fc069b581b2a6fe43572bae0d36937d
Taras <tarasparakhin@gmail.com>
2022-09-14 23:44:48 +0300

    directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewContainerRef} from '@angular/core';
+import {ItemComponent} from "./item/item.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angularScrinkast';
+
+  constructor(
+    private viewContainerRef: ViewContainerRef) {
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.viewContainerRef.createComponent(ItemComponent)
+    }, 3000)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ItemComponent } from './item/item.component';
+import { ColoryDirective } from './colory.directive';
+import { DelayDirective } from './delay.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    ColoryDirective,
+    DelayDirective
   ],
   imports: [
     BrowserModule
