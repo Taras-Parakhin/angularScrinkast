991a0586dae959918f95989445f891c0721699ba
Taras <tarasparakhin@gmail.com>
2022-09-16 01:28:05 +0300

    form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, Validators} from "@angular/forms";
+import {formControl} from "@angular/core/schematics/migrations/typed-forms/util";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angularScrinkast';
+  nameControl: FormControl;
+
+  ngOnInit() {
+    // this.nameControl = new FormControl<any>('John', [Validators.required, Validators.minLength(2)]);
+    this.nameControl = new FormControl<any>('John', [Validators.required, this.myValidator]);
+    this.nameControl.valueChanges.subscribe(value => console.log(value))
+    this.nameControl.statusChanges.subscribe(status => {
+      console.log(status);
+      console.log(this.nameControl.errors)
+    })
+  }
+
+  myValidator(formControl: FormControl) {
+    if (formControl.value.length < 3) {
+      return{myValidator: {message: 'Should be something'}};
+    }
+
+    return null;
+  }
 }
+
