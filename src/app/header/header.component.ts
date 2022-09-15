c8028d5edcb1221e6c3319dff53ec4eb2a6d2360
Taras <tarasparakhin@gmail.com>
2022-09-15 19:07:40 +0300

    directives

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from "../user-service/user.service";
+// import {IUser} from "../interfaces/user.interface";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  // public users: IUser[];
+  //
+  // constructor(private userService: UserService) { }
+  //
+  ngOnInit(): void {
+  //   this.users = this.userService.getAll()
+  }
+  //
+  // deleteUser(name: string) {
+  //   this.userService.delete(name);
+  //   this.users = this.userService.getAll()
+  // }
+  //
+  // addUser(name: string) {
+  //   this.userService.add(name);
+  // }
+}
---
+++ src/app/user-service/user.service.ts
@@ -0,0 +1,28 @@
+// import {IUser} from "../interfaces/user.interface";
+
+export class UserService {
+
+  // public users = [
+  //   {name: 'John'},
+  //   {name: 'Bob'},
+  //   {name: 'Nick'}
+  // ]
+  //
+  // constructor() { }
+  //
+  // getAll() {
+  //   return this.users;
+  // }
+  //
+  // delete(name: string): IUser[] {
+  //   this.users = this.users.filter(user => user.name !== name);
+  //   return this.users;
+  // }
+  //
+  // add(name: string) {
+  //   if (!name) {
+  //     return;
+  //   }
+  //   this.users.push({name})
+  // }
+}
