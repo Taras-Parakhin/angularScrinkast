ad8587a72325a8b1a0c25b0e965cecb760f249ed
Taras <tarasparakhin@gmail.com>
2022-09-16 12:12:44 +0300

    form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, Validators} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {Observable, of} from "rxjs";
 
 @Component({
@@ -10,15 +10,24 @@ import {Observable, of} from "rxjs";
 export class AppComponent implements OnInit{
   title = 'angularScrinkast';
   nameControl: FormControl;
+  form: true;
+  fullNameControl: FormGroup;
 
   ngOnInit() {
     // this.nameControl = new FormControl<any>('John', [Validators.required, Validators.minLength(2)]);
-    this.nameControl = new FormControl<any>('John', [Validators.required, this.myValidator], [this.myAsyncValidator]);
-    this.nameControl.valueChanges.subscribe(value => console.log(value))
-    this.nameControl.statusChanges.subscribe(status => {
-      console.log(status);
-      console.log(this.nameControl.errors)
-    })
+    // this.nameControl = new FormControl<any>('John', [Validators.required, this.myValidator], [this.myAsyncValidator]);
+    // this.nameControl.valueChanges.subscribe(value => console.log(value))
+    // this.nameControl.statusChanges.subscribe(status => {
+    //   console.log(status);
+    //   console.log(this.nameControl.errors)
+    // });
+
+    this.fullNameControl = new FormGroup({
+      firsName: new FormControl(),
+      lastName: new FormControl()
+    });
+
+    // this.fullNameControl.valueChanges.subscribe(value => console.log(value))
   }
 
   myValidator(formControl: FormControl) {
